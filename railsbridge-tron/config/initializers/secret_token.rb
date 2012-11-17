7f9cdc813dec7548059d2366f65d2a337859eadc
Derek Chen <derekchen14@gmail.com>
2012-11-16 20:35:45 -0800

    entering railsbridge into archives

---
+++ railsbridge-tron/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tron::Application.config.secret_token = 'd4cddce73e8b933ffdddc106a6fd7d3cced84d8bad2e33765de9c55de2665985793f0b57dd28b245441cf21043ee8bca0a419517081ff4794a62523f7cdd1da4'
